4137811e601ef29c927c7b4407fa8aefee33d98a
Yuki Honda <yucchi.mail1108@gmail.com>
2026-03-12 21:08:48 +0900

    レシピ詳細ページを追加

--- apps/recipes/urls.py
+++ apps/recipes/urls.py
@@ -5,4 +5,5 @@ app_name = 'recipes'
 
 urlpatterns = [
     path("", views.recipe_list_view, name="recipe_list"),
+    path("<int:recipe_id>", views.recipe_detail_view, name="recipe_detail"),
 ]
--- apps/recipes/views.py
+++ apps/recipes/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Recipe
 
 def recipe_list_view(request):
@@ -10,3 +10,14 @@ def recipe_list_view(request):
         {"recipes":recipes}    
     )
     
+def recipe_detail_view(request, recipe_id):
+    recipe = get_object_or_404(
+        Recipe,
+        id=recipe_id    
+    )
+    
+    return render(
+        request,
+        "recipes/recipe_detail.html",
+        {"recipe": recipe}
+    )
