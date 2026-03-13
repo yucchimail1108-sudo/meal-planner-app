d4bb35593f9ace1fcd4696541155c72e96010ba8
Yuki Honda <yucchi.mail1108@gmail.com>
2026-03-13 10:45:02 +0900

    Recipeをログインユーザーに紐づけ

--- apps/recipes/models.py
+++ apps/recipes/models.py
@@ -1,6 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Recipe(models.Model):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        verbose_name="ユーザー",
+        null=True,
+        blank=True
+    ) 
     recipe_name = models.CharField(
         max_length=100,
         verbose_name="・レシピ名"
--- apps/recipes/views.py
+++ apps/recipes/views.py
@@ -1,45 +1,35 @@
 from django.shortcuts import render, get_object_or_404,redirect
+from django.contrib.auth.decorators import login_required
 from .models import Recipe
 from .forms import RecipeForm
 
 # レシピ一覧画面
+@login_required
 def recipe_list_view(request):
-    recipes = Recipe.objects.all()
-
-    return render(
-        request,
-        "recipes/recipe_list.html",
-        {"recipes":recipes}    
-    )
+    recipes = Recipe.objects.filter(user=request.user)
+    
+    return render(request, "recipes/recipe_list.html", {"recipes":recipes})
 
 # レシピ詳細画面
 def recipe_detail_view(request, recipe_id):
-    recipe = get_object_or_404(
-        Recipe,
-        id=recipe_id    
-    )
+    recipe = get_object_or_404(Recipe,id=recipe_id)
     
-    return render(
-        request,
-        "recipes/recipe_detail.html",
-        {"recipe": recipe}
-    )
+    return render(request, "recipes/recipe_detail.html", {"recipe": recipe})
 
 # 登録画面
+@login_required
 def recipe_create_view(request):
     if request.method == 'POST':
         form = RecipeForm(request.POST)
         if form.is_valid():
-            form.save()
+            recipe = form.save(commit=False)
+            recipe.user = request.user
+            recipe.save()
             return redirect('recipes:recipe_list')
     else:
         form = RecipeForm()
         
-    return render(
-        request,
-        "recipes/recipe_form.html",
-        {"form": form}
-    )
+    return render(request, "recipes/recipe_form.html", {"form": form})
 
 # 編集画面
 def recipe_update_view(request, recipe_id):
@@ -53,11 +43,7 @@ def recipe_update_view(request, recipe_id):
     else:
         form = RecipeForm(instance=recipe)
         
-    return render(
-        request,
-        "recipes/recipe_form.html",
-        {"form": form}
-    )
+    return render(request, "recipes/recipe_form.html", {"form": form})
     
 # 削除画面
 def recipe_delete_view(request, recipe_id):
@@ -67,8 +53,4 @@ def recipe_delete_view(request, recipe_id):
         recipe.delete()
         return redirect('recipes:recipe_list')
            
-    return render(
-        request,
-        "recipes/recipe_confirm_delete.html",
-        {"recipe": recipe}
-    )
+    return render(request, "recipes/recipe_confirm_delete.html", {"recipe": recipe})
