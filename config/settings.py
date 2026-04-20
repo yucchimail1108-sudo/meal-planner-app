c205ab7ca0f6cf8c7b67ca170e33c7c5027935b3
Yuki Honda <yucchi.mail1108@gmail.com>
2026-04-20 22:03:16 +0900

    デプロイ前最終調整（よみがな対応・スクロール修正・ポートフォリオ遷移修正）

--- config/settings.py
+++ config/settings.py
@@ -11,9 +11,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-b6rmdn9yp%epux#+xmb11l6li5x(k=k$k!fne--88cuf4rsish'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -105,7 +105,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 STATICFILES_DIRS = [
     BASE_DIR / "static",
