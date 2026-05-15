42313d799b2600f120e21034f9da1fcc469ee966
Yuki Honda <yucchi.mail1108@gmail.com>
2026-05-16 00:46:28 +0900

    追加ボタン幅などUI調整

--- config/settings.py
+++ config/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-b6rmdn9yp%epux#+xmb11l6li5x(k=k$k!fne--88cuf4rsish'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG =False
+DEBUG False
 
 ALLOWED_HOSTS = [
     "yucchiapp.pythonanywhere.com",
@@ -24,7 +24,8 @@ ALLOWED_HOSTS = [
 
 INSTALLED_APPS = [
     'django.contrib.admin',
-    'django.contrib.auth',
+    'django.contrib.auth'
+    ,
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
